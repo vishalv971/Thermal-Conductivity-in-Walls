715463415393908380ab725dc5fd72c44dc47bac
Abhishek Patra <patrasap0908@gmail.com>
2018-01-19 08:05:57 +0530

    Arduino Codebase

---
+++ Max6675.ino
@@ -0,0 +1,27 @@
+#include "max6675.h"
+
+int ktcSO = 12;
+int ktcCS = 10;
+int ktcCLK = 13;
+
+MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
+
+  
+void setup() {
+  Serial.begin(9600);
+  // give the MAX a little time to settle
+  delay(500);
+     
+}
+
+void loop() {
+  // basic readout test
+  //delay(1000);
+  while(Serial.available()) {
+    int incomingByte = Serial.read();
+    //char input = Serial.parseInt();
+    //Serial.print(incomingByte);
+    Serial.println(ktc.readCelsius()); 
+  }
+}
+
